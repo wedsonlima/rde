fdcfe1b40441f69328c52fbc2756d723b79e0484
Wedson Lima <wedson.sousa.lima@gmail.com>
2015-09-06 12:17:16 -0300

    rvm / add gpg key

--- rvm-install.sh
+++ rvm-install.sh
@@ -15,6 +15,7 @@ postgresql-9.4 nodejs ssh
 # instalando rvm
 echo -e "\n\nInstalando rvm"
 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby; source $HOME/.rvm/scripts/rvm; rvm reload
 
 # load do rvm no shell como uma funcao
